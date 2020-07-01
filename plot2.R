f58cab418d25c8ad7acc82bde0517d7f3874b4fd
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-07-01 13:11:14 +0530

    plot 2 files added

---
+++ plot2.R
@@ -0,0 +1,17 @@
+# Reading the text file
+
+powerCondata <- read.csv("../exdata_data_household_power_consumption/household_power_consumption.txt",
+                         sep = ";",
+                         na.strings = "?" )
+nrow(powerCondata)
+
+subsetPowerData <-  subset(powerCondata, powerCondata$Date == "1/2/2007" | powerCondata$Date =="2/2/2007")
+
+str(subsetPowerData) 
+
+png("plot2.png", width=480, height=480)
+
+plot(strptime(paste(subsetPowerData $Date, subsetPowerData $Time, sep=" "), "%d/%m/%Y %H:%M:%S"), as.numeric(subsetPowerData $Global_active_power), type="l", xlab="", 
+     ylab="Global Active Power (kilowatts)")
+
+dev.off()
